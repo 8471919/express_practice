14375fd2bf84a1ecc48497d8285dea026c4ebb5b
8471919 <7471919@naver.com>
2021-07-18 19:32:33 +0900

    로그아웃 구현

--- router/main/main.js
+++ router/main/main.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+//main page는 login이 될 때만(즉, 세션정보가 있을때만) 접근이 가능하게 하자.
 router.get("/", (req, res, next) => {
     //req의 user라는 속성으로 deserialize의 id값에 접근 가능
-    console.log("main js loaded", req.user);
     const id = req.user;
+    if (!id) res.render("login.ejs");
 
-    // res.send("<h1>hi friend1</h>");
-    // res.sendFile(path.join(__dirname + "/../../public/main.html"));
     res.render("main.ejs", { id: id });
 });
 
