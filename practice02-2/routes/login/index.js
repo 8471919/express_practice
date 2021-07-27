3dd38971a521c66be07ddee7a654f3a1cffbb2fc
8471919 <7471919@naver.com>
2021-07-27 23:57:15 +0900

    중간실습02-2: 로그인구현

--- practice02-2/routes/login/index.js
+++ practice02-2/routes/login/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const mysql = require("mysql2");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -10,29 +12,90 @@ const connection = mysql.createConnection({
     database: "jsman",
 });
 
+passport.serializeUser((user, done) => {
+    console.log("passport session save : ", user.id);
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    console.log("passport session get id : ", id);
+    done(null, id);
+});
+
+passport.use(
+    "hansu-login",
+    new LocalStrategy(
+        {
+            usernameField: "email",
+            passwordField: "pw",
+            passReqToCallback: true,
+        },
+        (req, email, pw, done) => {
+            const query = connection.query(
+                `select * from user where email="${email}" LIMIT 1`,
+                (err, rows) => {
+                    console.log(req.body);
+                    if (err) return done(err);
+                    if (!rows.length)
+                        return done(null, false, {
+                            email,
+                            message: "해당 이메일은 존재하지 않습니다.",
+                            state: "fail",
+                        });
+                    if (rows[0].PW === pw) {
+                        return done(null, {
+                            message: `반갑습니다! ${rows[0].NAME}님!`,
+                            email,
+                            id: rows[0].UID,
+                            name: rows[0].NAME,
+                        });
+                    }
+                    return done(null, false, {
+                        message: "비밀번호가 다릅니다.",
+                        state: "fail",
+                    });
+                }
+            );
+        }
+    )
+);
+
 router.get("/", (req, res, next) => {
     console.log(req.user);
     res.render("login.ejs");
 });
 
 router.post("/", (req, res, next) => {
-    const resData = {};
-    const email = req.body.email;
-    const pw = req.body.pw;
-    console.log(email, pw);
-
-    const query = connection.query(
-        `select * from user where email="${email}" LIMIT 1`,
-        (err, rows) => {
-            if (err) throw err;
-            if (!rows.length) return res.status(200).json("OK");
-            if (rows[0].PW === pw) {
-                resData.email = email;
-                return res.json(resData);
-            }
-            return res.json(resData);
-        }
-    );
+    passport.authenticate("hansu-login", (err, user, info) => {
+        console.log(info);
+        console.log(user);
+        if (err) return res.status(500).json(err);
+        if (!user) return res.status(401).json(info);
+        req.logIn(user, (err) => {
+            if (err) return next(err);
+            return res.json(user);
+        });
+    })(req, res, next);
 });
+
+// router.post("/", (req, res, next) => {
+//     const resData = {};
+//     const email = req.body.email;
+//     const pw = req.body.pw;
+//     console.log(email, pw);
+
+//     const query = connection.query(
+//         `select * from user where email="${email}" LIMIT 1`,
+//         (err, rows) => {
+//             if (err) throw err;
+//             if (!rows.length) return res.status(200).json("OK");
+//             if (rows[0].PW === pw) {
+//                 resData.email = email;
+//                 return res.json(resData);
+//             }
+//             return res.json(resData);
+//         }
+//     );
+// });
 
 module.exports = router;
